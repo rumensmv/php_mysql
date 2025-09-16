45a16ad52b1e7612f01772612c7b7cf137818739
Rumen Simov <Rumen_Simov@etu.u-bourgogne.fr>
2025-09-16 10:08:58 +0200

    Ajout d'une clé password dans le tableau  de variables.php

--- contact.php
+++ contact.php
@@ -16,19 +16,22 @@
 
     <?php include_once('header.php'); ?>
         <h1>Contactez nous</h1>
-        <form action="submit_contact.php" method="POST" enctype="multipart/form-data>
+        <form action="submit_contact.php" method="POST" enctype="multipart/form-data">
             <div class="mb-3">
                 <label for="email" class="form-label">Email</label>
-                <input type="email" class="form-control" id="email" name="email" placeholder="Votre email" required>
+                <input type="email" class="form-control" id="email" name="email" aria-describedby="email-help">
+                <div id="email-help" class="form-text">Nous ne revendrons pas votre email.</div>
             </div>
             <div class="mb-3">
                 <label for="message" class="form-label">Votre message</label>
-                <textarea class="form-control" id="message" name="message" placeholder="Exprimez-vous" required></textarea>
+                <textarea class="form-control" placeholder="Exprimez vous" id="message" name="message"></textarea>
             </div>
+            <!-- Ajout champ d'upload ! -->
             <div class="mb-3">
-                <label for="screenshot" class="forl-label">Votre capture d'écran</label>
-                <input type="file" class="form-control" id="screenshot" name="screenshot"/>
+                <label for="screenshot" class="form-label">Votre capture d'écran</label>
+                <input type="file" class="form-control" id="screenshot" name="screenshot" />
             </div>
+            <!-- Fin ajout du champ -->
             <button type="submit" class="btn btn-primary">Envoyer</button>
         </form>
         <br />
