1250728a4ff9017827b93aaafe739f934cff5acf
Rumen Simov <Rumen_Simov@etu.u-bourgogne.fr>
2025-09-16 10:39:24 +0200

    Correction getRecipe

--- index.php
+++ index.php
@@ -29,11 +29,11 @@
 
     <!-- Si l'utilisateur existe, on affiche les recettes -->
     <?php if (isset($loggedUser)): ?>
-        <?php foreach (get_recipes($recipes, $limit) as $recipe) : ?>
+        <?php foreach (getRecipes($recipes) as $recipe) : ?>
             <article>
                 <h3><?php echo $recipe['title']; ?> </h3>
                 <div><?php echo $recipe['recipe']; ?> </div>
-                <i><?php echo display_author($recipe['author'], $users); ?> </i>
+                <i><?php echo displayAuthor($recipe['author'], $users); ?> </i>
             </article>
         <?php endforeach ?>
     <?php endif; ?>
